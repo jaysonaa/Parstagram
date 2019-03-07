b51b9c591a52fa14f585bd2941d02a343c7ad709
Jay-son Alburg <Jay-son@jay-sons-macbook-pro.local>
2019-03-06 19:21:41 -0500

    instagram part 1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.clientKey = "112233445566"
+                configuration.server = "https://protected-sierra-26401.herokuapp.com/parse"
+            })
+        )
+        
+        
         return true
     }
 
